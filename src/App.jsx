a402cd4c7aae0eed81e98408af56997694852a04
srihari-m-s <srihari.magadi@gmail.com>
2023-08-22 13:30:40 +0530

    feat:Added welcome and selection

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,19 @@ function App() {
 
   return (
     <>
-      <header>
-        <div className="container-lg">
-          This is the header
-        </div>
-      </header>
-
       <main className='bg-image'>
-        <div className='container-lg'>
+        <div className='container flex-col'>
+
+          <div className="welcome">
+            <h1>welcome to lotr info</h1>
+          </div>
           
+          <div className="selection">
+            <button className="selector">Books</button>
+            <button className="selector">Movies</button>
+            <button className="selector">Characters</button>
+          </div>
+
         </div>
       </main>
     </>
