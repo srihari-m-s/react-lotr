e053544210170b6d37cf5b4a4bbcd04d555c3685
srihari-m-s <srihari.magadi@gmail.com>
2023-08-23 22:13:11 +0530

    Added characters display and search functionality

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useState, useMemo } from 'react'
 
 export default function Characters({data}) {
-  return (
-    <div className='display-card'>
-        Characters
-    </div>
-  )
+
+    const [search, setSearch] = useState("")
+    
+    function filterArray(search){
+        if(search === ""){
+            return data.docs
+        } else {
+            return (data.docs.filter(char => char.name.toLowerCase().includes(search.toLowerCase())))
+        }
+    }
+
+    const visibleChars = useMemo(() => filterArray(search), [search])
+
+    let charsData = visibleChars.map((char, index) => {
+
+        return (
+            <div className="display-card" key={`${char["_id"]}`}>
+                
+                <h1 className='t-shadow'>{index+1}. {char.name}</h1>
+                {Object.keys(char).map((key, ind) => {
+                    if(key !== "name" && key !== "_id" && char[key] !== "" && char[key] !== "NaN"){
+
+                        if(key === "wikiUrl"){
+                            return (
+                                <p className='t-shadow charInfo' key={`${key}${ind}`}>{key}: <a href={char[key]} target='_blank' className="charLink" rel="noopener noreferrer">{char[key]}</a></p>
+                            )
+                        } else {
+
+                            return (
+                                <p className='t-shadow charInfo' key={`${key}${ind}`}>{key}: {char[key]}</p>
+                            )
+                        }
+                    }
+                })}
+                
+            </div>
+        )
+    })
+
+    return (
+        <> 
+            <input type="search" name="charSearch" id="charSearch" className='charSearch' autoFocus placeholder='Search by name...' onInput={e => setSearch(e.target.value)}/>
+            {charsData}
+        </>
+    )
 }
